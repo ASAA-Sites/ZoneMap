f51646de31bbe396d392185e57e0be197ac6bfa8
Casey Clarke <casey.noam.clarke@gmail.com>
2024-08-13 12:30:09 -0600

    Fixed problems with embedding

--- js/app.js
+++ js/app.js
@@ -6,3 +6,16 @@ $('.multi-link').on('click', function() {
         window.location.assign("links.html?location=" +  $(this).attr('alt'));
         return false;
 });
+
+
+
+// $('.single-link').on('click', function() {
+//     e.preventDefault();
+//     window.open("example.com");
+//     return false;
+// });
+
+
+function redirect(link) {
+    window.open(link)
+}
