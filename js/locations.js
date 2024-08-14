ca71de011da68736b010e6b42b38ad04de985349
Casey Clarke <casey.noam.clarke@gmail.com>
2024-08-14 16:14:31 -0600

    added north central

--- js/locations.js
+++ js/locations.js
@@ -1,11 +1,14 @@
 let locations = {
     "North West" :    [ { "link" :  "https://sites.google.com/fvsd.ab.ca/north-west-alberta-schools-ath/home", "name" :  "NWASAA Website"}],
     "North East" :    [{"link" : "https://www.neasaa.org/", "name" : "NEASAA Website"}],
-    "North Central" : [],
+    "North Central" : [{"link" : "https://sites.google.com/gypsd.ca/northcentralzone", "name" : "North Central Website"}],
     "Edmonton" :      [ {"link" : "http://metroathletics.ca", "name" : "Metro Website"}],
     "Central" :       [],
     "South Central" : [{"link" : "https://southcentralzone.com/", "name" : "South Central Website"}],
-    "Calgary" :       [{"link" : "http://calgaryhighschoolsports.ca/", "name" : "CSHSAA Website"}, {"link" : "https://www.leaguelineup.com/welcome.asp?url=isaasr", "name" : "CISAA Website"}],
+    "Calgary" :       [
+        {"link" : "http://calgaryhighschoolsports.ca/", "name" : "CSHSAA Website"}, 
+        {"link" : "https://www.leaguelineup.com/welcome.asp?url=isaasr", "name" : "CISAA Website"}
+    ],
     "South" :         [
         {"link" : "https://saiac.schoolsites.ca/", "name" : "SAIAC Website"}, 
         {"link" : "https://sites.google.com/view/sebcbasketball/home", "name" : "SEBC Website"}, 
